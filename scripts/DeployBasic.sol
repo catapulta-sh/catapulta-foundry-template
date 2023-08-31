4e9b7ff0fb5d76891a0a24a63d570c4a360dc00f
kartojal <canillas.mail@gmail.com>
2023-08-31 17:36:06 +0200

    feat: remove multi deployment to keep simple

--- scripts/DeployBasic.sol
+++ scripts/DeployBasic.sol
@@ -2,20 +2,15 @@
 pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
-import {MultiDeployment} from "../src/MultiDeployment.sol";
 import {Basic} from "../src/Basic.sol";
 
-contract Deploy is Script {
-    function run() external {
+contract DeployBasic is Script {
+    function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
 
         new Basic(1);
-
-        MultiDeployment test = new MultiDeployment(5);
-
-        test.testFactoryDeployment();
 
         vm.stopBroadcast();
     }
