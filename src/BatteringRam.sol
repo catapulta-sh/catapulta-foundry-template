0fb4ad8a8c2d3544808544f43fa43c2d393a7ff2
kartojal <canillas.mail@gmail.com>
2023-11-07 15:23:18 +0100

    feat: add champions

--- src/BatteringRam.sol
+++ src/BatteringRam.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import {Champion} from "./Champion.sol";
 
 contract BatteringRam {
     uint256 internal _damage;
 
     constructor(uint256 damage) {
         _damage = damage;
+
+        new Champion();
+        new Champion();
+        new Champion();
     }
 
     function getAttackDamage() external view returns (uint256) {
---
+++ src/Champion.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Champion {
+    uint256 internal constant _damage = 2;
+
+    function getAttackDamage() external pure returns (uint256) {
+        return _damage;
+    }
+
+    function attack() external pure returns (string memory) {
+        return "plof!";
+    }
+}
