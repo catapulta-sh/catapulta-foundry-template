25025acca5623446394e8bf9f7f77e1efe861f54
kartojal <canillas.mail@gmail.com>
2023-12-29 16:26:36 +0100

    feat: add single deployment

---
+++ script/Trebuchet.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {Trebuchet} from "../src/Trebuchet.sol";
+
+contract DeployTrebuchet is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        new Trebuchet(200);
+
+        vm.stopBroadcast();
+    }
+}
